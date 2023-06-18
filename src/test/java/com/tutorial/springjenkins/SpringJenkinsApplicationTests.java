fea235b38b4abc507456589519d57176f45fc948
Mukonazwothe <rambuwanimukonazwothe@gmail.com>
2023-06-18 21:46:11 +0200

    Adding junit test

--- src/test/java/com/tutorial/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/tutorial/springjenkins/SpringJenkinsApplicationTests.java
@@ -1,13 +1,22 @@
 package com.tutorial.springjenkins;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class SpringJenkinsApplicationTests {
-
+	public static Logger logger = Logger.getLogger(SpringJenkinsApplicationTests.class.getName());
 	@Test
 	void contextLoads() {
+		logger.info("Testing case pipeline...");
+		assertEquals(true, true);
 	}
 
 }
